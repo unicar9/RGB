8614f3ef1d207bb01a79490ce59f7984812edf71
Unicar <unicar9@gmail.com>
2019-02-16 15:56:46 +0800

    Added starter and finished folders

---
+++ RGB-starter/main.js
@@ -0,0 +1,16 @@
+
+function setup() {
+    createCanvas(window.innerWidth, window.innerHeight)
+}
+
+function draw() {
+
+}
+
+function mousePressed() {
+
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight)
+}
